0b0c92b78570437331af7ed8a3b7ae2255c9a8fe
luestevam <luestevam@hotmail.com>
2021-04-26 11:49:45 -0300

    Iniciando o código

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+//home é um parametro
+void main (){
+
+  runApp(MaterialApp(
+   title: "Frases do Dia",
+    home: Column(
+      children:<Widget> [
+        Text("texto1"),
+        Text("Texto2"),
+        Text("Texto3")
+
+
+        ],
+      ),
+
+
+  ));
+
+
+}
